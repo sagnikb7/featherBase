3d2ed414cf82ab1e4ba5258d092fe2249770b205
sagnikbetal <sagnikbetal@gofynd.com>
2025-06-20 19:57:56 +0530

    optimised Helmet

--- src/middlewares/basic.js
+++ src/middlewares/basic.js
@@ -14,7 +14,28 @@ const basicMiddleware = (app) => {
   logger.info('adding basic middlewares');
   // basic safety
   app.use(timeout('30s'));
-  app.use(helmet());
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'"], // Avoid 'unsafe-inline' unless absolutely necessary
+          styleSrc: ["'self'", "'unsafe-inline'"], // Allow inline styles if used
+          imgSrc: [
+            "'self'",
+            'data:',
+            'https://cdn.download.ams.birds.cornell.edu', // allow Cornell bird images
+          ],
+          connectSrc: ["'self'"],
+          fontSrc: ["'self'", 'https:', 'data:'],
+          objectSrc: ["'none'"],
+          frameAncestors: ["'none'"],
+          upgradeInsecureRequests: [],
+        },
+      },
+      crossOriginEmbedderPolicy: false, // optional: if you get COEP-related errors
+    }),
+  );
 
   // body-parsers
   app.use(express.json());
